38ad3f89ac179d89eece7c7fc4899e2e8d1d520c
baikaifa <1956841815@qq.com>
2019-06-06 11:06:14 +0800

    修改第一个tabBar

--- 蜗牛图书/datas/list-data.js
+++ 蜗牛图书/datas/list-data.js
@@ -3,7 +3,7 @@ let list_data = [
   {
 
     date: 'may 19 2018',
-    title: '教育理念',
+    title: 'nodejs',
     detail_img: '/images/detail/carousel/02.jpg',
     avatar: '/images/avatar/4.png',
     detail_content: '纯正商业级应用 Node.js Koa2开发微信小程序服务端Node.js作为前端同学进阶的必修技术之一，最近这几年非常流行。现在的前端和服务端的界限已变的非常模糊，前端同学无论从独立完成项目的角度，还是从进阶全栈提升自己的角度，Node.js都是非常不错的学习方向',
@@ -47,7 +47,7 @@ let list_data = [
   {
 
     date: 'may 19 2018',
-    title: '学科介绍',
+    title: 'Tensorflow',
     detail_img: '/images/detail/carousel/03.jpg',
     avatar: '/images/avatar/4.png',
     detail_content: '课程以Tensorflow2.0框架为主体，以图像分类、房价预测、文本分类、文本生成、机器翻译、泰坦尼克生存预测等项目为依托，讲解Tensorflow框架的使用方法，让学员获得灵活使用Tensorflow的能力，同时学习到相关的深度学习/机器学习知识，达到初级深度学习工程师的水平',
@@ -68,13 +68,13 @@ let list_data = [
   },
   {
     date: '2018/3/15 下午 4:30:35',
-    title: '贾静雯简介',
-    detail_img: '/images/detail/list/j2.jpg',
-    detail_content: '贾静雯（Alyssa Chia），1974年10月7日出生于台湾省台北市，祖籍天津市，华语影视女演员、节目主持人。1990年，贾静雯因接拍爱之味妞妞甜八宝广告而出道演艺圈；同年，她还出演了个人的首部电视剧《佳家福》。1994年，贾静雯开始担任一系列儿童节目的主持人。1997年，她凭借家庭剧《四千金》获得台湾电视金钟奖最佳新人奖[1]  。2000年，贾静雯主演的古装剧《飞龙在天》获得了台湾电视剧年度收视冠军。',
+    title: 'Java电商秒杀系统深度优化 从容应对亿级流量挑战',
+    detail_img: '/images/detail/carousel/04.jpg',
+    detail_content: '课程中通过多种性能优化技术，总结了互联网项目中的经典性能优化方案技术，提供了统一的设计思维和方案思考方式，提升学员思维。真正理解性能优化中每个技术的使用以及背后的原理',
     detail_love_image1: '/images/icon/chat.png',
     detail_love_image2: '/images/icon/view.png',
     love_count: 92,
-    headImgSrc: '/images/detail/list/j2.jpg',
+    headImgSrc: '/images/detail/carousel/04.jpg',
     author: '新华社',
     attention_count: 88,
     avatar: '/images/avatar/1.png',
@@ -84,68 +84,7 @@ let list_data = [
       coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
     },
     postId: 3
-  },
-  {
-    date: '2018/3/17 下午3:30:35',
-    title: '贾静雯作品展',
-    detail_img: '/images/detail/list/j3.jpg',
-    detail_content: '2001年，贾静雯将工作重心转向内地，并主演了传奇剧《大汉天子》。2002年，她凭借武侠剧《倚天屠龙记》在内地赢得更高关注度[2]  。2003年，贾静雯获得FHM全球百大性感美女亚洲区冠军。2006年，他主演了现代剧《悲伤时唱首歌》。此后几年，贾静雯相继出演了传奇剧《太平公主秘史》、剧情片《不能说的夏天》等影视作品',
-    detail_love_image1: '/images/icon/chat.png',
-    detail_love_image2: '/images/icon/view.png',
-    love_count: 88,
-    attention_count: 66,
-    headImgSrc: '/images/detail/list/j3.jpg',
-    author: '新华社',
-    avatar: '/images/avatar/2.png',
-    music: {
-      dataUrl: 'http://up.mcyt.net/down/46102.mp3', // 音乐链接
-      title: '汪峰 - 儿时',   // 音乐标题
-      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
-    },
-    postId: 4
-  },
-  {
-
-    date: 'sep 19 2016',
-    title: '娱乐新闻',
-    detail_img: '/images/detail/list/j4.jpg',
-    avatar: '/images/avatar/3.png',
-    detail_content: '2014年，贾静雯与小自己九岁的台湾演员修杰楷相恋[41]  。2015年5月5日，贾静雯的男友修杰楷在微博宣布了贾静雯已身怀有孕的消息，并发出了他与贾静雯和梧桐妹的两张合照，随后，贾静雯转发了此条微博并希望得到大家的祝福[42]  ；同年8月7日，贾静雯以剖宫产方式生下了与修杰楷的第一个女儿“咘咘”，而此前两人已正式注册结婚[43]  。',
-    headImgSrc: '/images/detail/list/j4.jpg',
-    author: '李白3',
-    detail_love_image1: '/images/icon/chat.png',
-    detail_love_image2: '/images/icon/view.png',
-    love_count: 88,
-    attention_count: 66,
-    detail: '女神。。。',
-    music: {
-      dataUrl: 'http://up.mcyt.net/down/46110.mp3', // 音乐链接
-      title: '曲婉婷 - 我的歌声里-(电视剧《在线爱》主题曲)',   // 音乐标题
-      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
-    },
-    postId: 5
-  },
-  {
-
-    date: 'sep 19 2016',
-    title: '社会活动',
-    detail_img: '/images/detail/list/j6.jpg',
-    avatar: '/images/avatar/4.png',
-    detail_content: '2012年，贾静雯担任公益大使，发起为弱势儿童和青少年课后照护计划，并为此次活动献唱了个人单曲《许一个愿望》。2014年，贾静雯参加了最终梦想年度时尚魅力女性颁奖盛典，并在典礼上获得了亲情天使奖[50] ',
-    headImgSrc: '/images/detail/list/j6.jpg',
-    author: '新华社',
-    detail_love_image1: '/images/icon/chat.png',
-    detail_love_image2: '/images/icon/view.png',
-    love_count: 88,
-    attention_count: 66,
-    detail: '女神2。。。',
-    music: {
-      dataUrl: 'http://up.mcyt.net/down/46100.mp3', // 音乐链接
-      title: 'IF-Ken Arai',   // 音乐标题
-      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
-    },
-    postId: 6
-  },
+  }
 ];
 
 module.exports = {list_data};
