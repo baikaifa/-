2a6c971890aa5d49dd8b3c607bd5858f9158ce34
baikaifa <1956841815@qq.com>
2019-06-06 10:45:07 +0800

    名字无法修改 蜗牛图书就是项目名称

--- 蜗牛图书/datas/list-data.js
+++ 蜗牛图书/datas/list-data.js
@@ -6,7 +6,7 @@ let list_data = [
     title: '教育理念',
     detail_img: '/images/detail/carousel/02.jpg',
     avatar: '/images/avatar/4.png',
-    detail_content: '让每一个 IT 人，都直接或间接地受到过尚硅谷帮助！尚硅谷由一群有活力、有理想、有责任、有担当的尚硅谷人组成，我们将不负青春，为传播 IT 技术、为软件行业的健康的生态发展贡献力量。',
+    detail_content: '纯正商业级应用 Node.js Koa2开发微信小程序服务端Node.js作为前端同学进阶的必修技术之一，最近这几年非常流行。现在的前端和服务端的界限已变的非常模糊，前端同学无论从独立完成项目的角度，还是从进阶全栈提升自己的角度，Node.js都是非常不错的学习方向',
     headImgSrc: '/images/detail/carousel/02.jpg',
     author: '钢铁侠 ',
     dataTime: '24time',
@@ -24,11 +24,11 @@ let list_data = [
   },
   {
     
-    date: 'may 19 2018',
-    title: '尚硅谷',
+    date: 'may 19 2019',
+    title: '从算法应用出发，Python3带你买入机器学习领域首站',
     detail_img: '/images/detail/carousel/01.jpg',
     avatar: '/images/avatar/4.png',
-    detail_content: '硅谷IT教育隶属于北京晟程华科教育科技有限公司，是国内领先的专业IT教育培训机构，拥有北京、深圳两处基地。自2013年成立以来，凭借领先的教育理念、前沿的课程体系、优秀的教学团队、科学的考评制度、严格的教务管理、完备的就业保障，已经为行业输送了万余名高端技术人才。',
+    detail_content: '欢迎大家来到《Python3玩转机器学习》的课堂。在这个课程中，我们将从0开始，一点一点进入机器学习的世界。本门课程对机器学习领域的学习，绝不不仅仅只是对算法的学习，还包括诸如算法的评价，方法的选择，模型的优化，参数的调整，数据的整理，等等一系列工作。准备好了吗？现在开始我们的机器学习之旅！...',
     headImgSrc: '/images/detail/carousel/01.jpg',
     author: '美国队长',
     dataTime: '24time',
@@ -38,8 +38,8 @@ let list_data = [
     attention_count: 66,
     detail: '有魅力的老男人。',
     music: {
-      dataUrl: 'http://www.ytmp3.cn/down/50395.mp3', // 音乐链接
-      title: '一路向北',   // 音乐标题
+      dataUrl: 'http://up.mcyt.net/down/46100.mp3', // 音乐链接
+      title: 'up',   // 音乐标题
       coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
     },
     postId: 1
@@ -50,7 +50,7 @@ let list_data = [
     title: '学科介绍',
     detail_img: '/images/detail/carousel/03.jpg',
     avatar: '/images/avatar/4.png',
-    detail_content: '尚硅谷现开设JavaEE+大数据、HTML5前端+全栈、大数据+机器学习、Python+人工智能、Android+HTML5混合开发等多门学科；同时，通过视频分享、谷粒学院在线课堂、直播课堂等多种方式，满足了全国编程爱好者对多样化学习场景的需求。目前，尚硅谷“谷粉”人数已超500万，面授班学员绝大多数都在北上广深等一线城市高薪就业，就业薪资更是屡创新高！',
+    detail_content: '课程以Tensorflow2.0框架为主体，以图像分类、房价预测、文本分类、文本生成、机器翻译、泰坦尼克生存预测等项目为依托，讲解Tensorflow框架的使用方法，让学员获得灵活使用Tensorflow的能力，同时学习到相关的深度学习/机器学习知识，达到初级深度学习工程师的水平',
     headImgSrc: '/images/detail/carousel/03.jpg',
     author: '绿巨人',
     dataTime: '24time',
